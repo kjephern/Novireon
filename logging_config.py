90ef30e848eab065831d3c83951db77bfb51338b
rexkung1029 <rexkung1029@gmail.com>
2025-10-31 20:01:43 +0800

    Update logging_config.py

--- logging_config.py
+++ logging_config.py
@@ -17,15 +17,11 @@ def setup_logging():
     # --- 設定控制台輸出 (StreamHandler) ---
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(log_format)
-    # 您可以為不同的 handler 設定不同的層級
-    # 例如，控制台只顯示 INFO 以上的訊息
     console_handler.setLevel(logging.INFO)
 
     # --- 設定檔案輸出 (FileHandler) ---
-    # 這會將日誌寫入一個名為 app.log 的檔案中
-    file_handler = logging.FileHandler("app.log", mode="w")  # 'w' 表示每次執行都覆寫
+    file_handler = logging.FileHandler("app.log", mode="w")
     file_handler.setFormatter(log_format)
-    # 檔案中記錄所有 DEBUG 以上的訊息
     file_handler.setLevel(logging.DEBUG)
 
     # 將 handler 加入到根記錄器

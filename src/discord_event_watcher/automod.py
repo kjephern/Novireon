8bfef7456cc848b5e5f3669b74ba605fac2b5670
rexkung1029 <rexkung1029@gmail.com>
2025-11-03 18:38:47 +0800

    Create automod.py

---
+++ src/discord_event_watcher/automod.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("discord_event_watcher.automod")
+
+
+class AutomodWatcher:
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_automod_rule_create(self, rule: discord.AutoModRule):
+        pass
+
+    @commands.Cog.listener()
+    async def on_automod_rule_update(self, rule: discord.AutoModRule):
+        pass
+
+    @commands.Cog.listener()
+    async def on_automod_rule_delete(self, rule: discord.AutoModRule):
+        pass
+
+    @commands.Cog.listener()
+    async def on_automod_action(self, execution: discord.AutoModAction):
+        pass

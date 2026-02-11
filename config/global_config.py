0a60cc4c2ddf30497a9bd6acec3b0f43e260e6c2
rexkung1029 <rexkung1029@gmail.com>
2026-02-11 23:27:14 +0800

    restructure config

--- config/global_config.py
+++ config/global_config.py
@@ -6,12 +6,3 @@ DISCORD_DEFAULT_AVATAR = os.path.join(
     PROJECT_ROOT, "data", "default_discord_user_avatar.png"
 )
 DEFAULT_AVATAR = os.path.join(PROJECT_ROOT, "data", "default_avatar.jpg")
-
-
-class MusicConfig:
-    PLAYABACK_STATE_UPDATER_INTERVAL = 5  # seconds
-    MAX_SONGS_ADD_VIA_PLAYLIST = 20
-    MAX_SHOWED_SONGS_IN_LIST_QUEUE = 10  # recommend <=20
-    ADD_TO_QUEUE_COLOR = 0x28FF28
-    LIST_QUEUE_COLOR = 0xFFCF40
-    PLAYING_COLOR = 0xADC8FF

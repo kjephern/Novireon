4cb795e950b15a66219aea0803027d11f7af3e18
rexkung1029 <rexkung1029@gmail.com>
2026-02-12 16:11:58 +0800

    fix path

--- config/global_config.py
+++ config/global_config.py
@@ -1,8 +1,6 @@
-import os
+from pathlib import Path
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-FONT_PATH = os.path.join(PROJECT_ROOT, "data", "GenSenRounded2-M.ttc")
-DISCORD_DEFAULT_AVATAR = os.path.join(
-    PROJECT_ROOT, "data", "default_discord_user_avatar.png"
-)
-DEFAULT_AVATAR = os.path.join(PROJECT_ROOT, "data", "default_avatar.jpg")
+PROJECT_ROOT = Path(__file__).parent.parent
+FONT_PATH = PROJECT_ROOT / "data" / "GenSenRounded2-M.ttc"
+DISCORD_DEFAULT_AVATAR = PROJECT_ROOT / "data" / "default_discord_user_avatar.png"
+DEFAULT_AVATAR = PROJECT_ROOT / "data" / "default_avatar.jpg"

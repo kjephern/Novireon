67cd73867d07b1392c99b847d16617cf63a804e0
rexkung1029 <rexkung1029@gmail.com>
2026-02-12 16:13:18 +0800

    fix font path and add font License

--- config/global_config.py
+++ config/global_config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent.parent
-FONT_PATH = PROJECT_ROOT / "data" / "GenSenRounded2-M.ttc"
+FONT_PATH = PROJECT_ROOT / "data" / "font" / "gensen-font" / "GenSenRounded2-M.ttc"
 DISCORD_DEFAULT_AVATAR = PROJECT_ROOT / "data" / "default_discord_user_avatar.png"
 DEFAULT_AVATAR = PROJECT_ROOT / "data" / "default_avatar.jpg"
